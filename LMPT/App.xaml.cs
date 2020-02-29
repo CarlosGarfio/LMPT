5fe5af35b0c7d47ff85c28e7b244611e5f30c33b
Carlos Garfio <gmcarlosd@outlook.com>
2020-02-29 16:52:54 -0700

    LoginPage DESIGN

---
+++ LMPT/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using LMPT.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LMPT
+{
+    public partial class App : Application
+    {
+        #region Constructores
+        public App()
+        {
+            InitializeComponent();
+
+            this.MainPage = new LoginPage();
+        }
+        #endregion
+        #region Metodos
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        } 
+        #endregion
+    }
+}
